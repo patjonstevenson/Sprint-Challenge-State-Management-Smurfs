28aa08405966710a640c6b469de98f85d9f3a421
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-11 09:35:50 -0700

    fleshed out actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,10 +1,36 @@
-const FETCH_SMURF = "FETCH_SMURF";
-const FETCH_SUCCESS = "FETCH_SUCCESS";
-const FETCH_FAILURE = "FETCH_FAILURE";
-const POST_SMURF = "POST_SMURF";
-const POST_SUCCESS = "POST_SUCCESS";
-const POST_FAILURE = "POST_FAILURE";
+import axios from "axios";
 
-const fetchSmurf = () => {
-    dispatch("FETCH_SMURF")
+export const FETCH_SMURF = "FETCH_SMURF";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
+export const POST_SMURF = "POST_SMURF";
+export const POST_SUCCESS = "POST_SUCCESS";
+export const POST_FAILURE = "POST_FAILURE";
+
+export const fetchSmurf = () => {
+    dispatch({ type: "FETCH_SMURF" });
+    axios
+        .get("http://localhost:3333/smurfs")
+        .then(res => {
+            console.log("Fetch success!\n", res);
+            dispatch({ type: FETCH_SUCCESS, payload: res })
+        })
+        .catch(err => {
+            console.log("Oops! Something went wrong fetching data:\n", err);
+            dispatch({ type: FETCH_FAILURE, payload: err });
+        });
+}
+
+export const postSmurf = smurf => {
+    dispatch({ type: "post_SMURF" });
+    axios
+        .post("http://localhost:3333/smurfs", smurf)
+        .then(res => {
+            console.log("Post success!\n", res);
+            dispatch({ type: FETCH_SUCCESS, payload: res })
+        })
+        .catch(err => {
+            console.log("Oops! Something went wrong posting data:\n", err);
+            dispatch({ type: FETCH_FAILURE, payload: err });
+        });
 }
